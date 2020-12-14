3af5bd9b01ef2cc03fe64db3e65014692efb87ba
Jordi Garcia Garrido <jordi.garcia@capside.com>
2020-12-14 10:32:25 +0100

    https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/220px-Trollface_non-free.png

---
+++ 10/run.py
@@ -0,0 +1,21 @@
+file1 = open('input.txt', 'r')
+Lines = file1.readlines()
+adapters = []
+for line in Lines:
+    line = line.strip("\n")
+    adapters.append(int(line))
+adapters.append(0)
+adapters.append(max(adapters)+3)
+adapters.sort()
+dif1 = 0
+dif2 = 0
+dif3 = 0
+for i in range(1,len(adapters)):
+    if adapters[i]-adapters[i-1]==1:
+        dif1 +=1
+    if adapters[i]-adapters[i-1]==2:
+        dif2 +=1
+    if adapters[i]-adapters[i-1]==3:
+        dif3 +=1
+
+print(f"{dif1} * {dif3} = {dif1 * dif3}")
