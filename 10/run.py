5a4bfd8b12b31e4aab3eaf3d655027151a8a9fd4
Jordi Garcia Garrido <jordi.garcia@capside.com>
2020-12-14 10:35:47 +0100

    Puzzle 10. Part 1

--- 10/run.py
+++ 10/run.py
@@ -10,6 +10,8 @@ adapters.sort()
 dif1 = 0
 dif2 = 0
 dif3 = 0
+
+#Part1
 for i in range(1,len(adapters)):
     if adapters[i]-adapters[i-1]==1:
         dif1 +=1
@@ -18,4 +20,4 @@ for i in range(1,len(adapters)):
     if adapters[i]-adapters[i-1]==3:
         dif3 +=1
 
-print(f"{dif1} * {dif3} = {dif1 * dif3}")
+print(f"Result Part 1: {dif1} * {dif3} = {dif1 * dif3}")
