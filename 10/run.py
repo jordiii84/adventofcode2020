ba605b4be8edede6fbe35a45ba5718cbe2c7a82b
Jordi Garcia Garrido <jordi.garcia@capside.com>
2020-12-15 19:16:23 +0100

    Puzzle 10. Part 2

--- 10/run.py
+++ 10/run.py
@@ -8,16 +8,23 @@ adapters.append(0)
 adapters.append(max(adapters)+3)
 adapters.sort()
 dif1 = 0
-dif2 = 0
 dif3 = 0
-
 #Part1
 for i in range(1,len(adapters)):
     if adapters[i]-adapters[i-1]==1:
         dif1 +=1
-    if adapters[i]-adapters[i-1]==2:
-        dif2 +=1
     if adapters[i]-adapters[i-1]==3:
         dif3 +=1
 
 print(f"Result Part 1: {dif1} * {dif3} = {dif1 * dif3}")
+
+#Part 2. Thanks to kaur_virunurm
+# https://www.reddit.com/r/adventofcode/comments/ka8z8x/2020_day_10_solutions/gfbo61q/?utm_source=reddit&utm_medium=web2x&context=3
+from collections import Counter
+c = Counter({0:1})
+for x in adapters:
+    c[x+1] += c[x]
+    c[x+2] += c[x]
+    c[x+3] += c[x]
+
+print("Part 2:", c[max(adapters) + 3])
